aebbffac98f7153c5ff437e436cd064a704596a7
opechapo <abolajiopeyemi000@gmail.com>
2026-04-21 23:03:37 +0100

    footers

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 // Import Logo Image
-import tradenixPro from "../assets/Images/tradenixPro.png";
+import tradenixPro from "../assets/Images/landingpage/tradenixpro.png";
 
 const Footer = () => {
   return (
